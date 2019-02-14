1790fc942e580c09e24516f70b203b30b9fb8b4d
hkuser01 <46618154+hkuser01@users.noreply.github.com>
2019-02-14 23:02:30 +0800

    Update Celts.vue

--- pages/civs/Celts.vue
+++ pages/civs/Celts.vue
@@ -8,16 +8,16 @@
       <div class="col-md-8">
         <div class="n1">
           <p>
-            <nuxt-link to="civs/Celts">賽爾特</nuxt-link>或稱凱爾特，是西歐的步兵和攻城器文明，世紀帝國II的可使用的最初文明之一。
+            <nuxt-link to="civs/Celts">賽爾特</nuxt-link>或稱凱爾特，是西歐的<nuxt-link to="units/Infantry_Units">步兵</nuxt-link>和<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>文明，世紀帝國II的可使用的最初文明之一。
           </p>
           <p>
             <nuxt-link to="civs/Celts">賽爾特</nuxt-link>是用蘇格蘭、爾蘭人、威爾士人為基礎的文明。但語言是愛爾蘭蓋爾語。活耀於整個西歐歷經蠻多次遷移。
           </p>
           <p>
-            <nuxt-link to="civs/Celts">賽爾特</nuxt-link>特殊單位是菘藍武士，真實歷史上有的一種兵為了讓自己更可怕會在臉上塗上藍色的漆(用菘藍這種植物製成的)，賽爾特人在十四歲時就會讓男孩上戰場也是因應當時的大量部落戰爭，從小就常在戰爭因此培育出面對戰爭的勇氣和作戰熱情，帝王時代的特殊科技賽爾特狂暴就是取自於這，然而在對蘇格蘭的戰爭中賽爾特用游擊戰造成蘇格蘭的麻煩，因此菘藍武士得特色也取自游擊戰要的速度/破壞，菘藍武士從赤裸上身/臉上藍漆..等都是有考究歷史的。
+            <nuxt-link to="civs/Celts">賽爾特</nuxt-link><nuxt-link to="units/Unique_Unit">特殊單位</nuxt-link>是<nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>，真實歷史上有的一種兵為了讓自己更可怕會在臉上塗上藍色的漆(用菘藍這種植物製成的)，賽爾特人在十四歲時就會讓男孩上戰場也是因應當時的大量部落戰爭，從小就常在戰爭因此培育出面對戰爭的勇氣和作戰熱情，帝王時代的<nuxt-link to="techs/untech">特殊科技</nuxt-link><nuxt-link to="techs/Furor_Celtica">賽爾特狂暴</nuxt-link>就是取自於這，然而在對蘇格蘭的戰爭中賽爾特用游擊戰造成蘇格蘭的麻煩，因此<nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>得特色也取自游擊戰要的速度/破壞，<nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>從赤裸上身/臉上藍漆..等都是有考究歷史的。
           </p>
           <p>
-            歷史上他們木工與金屬加工的優異，因而提高了攻城器製造所工作效率/伐木速度/攻城武器的精良，因離英格蘭很近在牧羊技術上也有相當的程度使得羊不易被順手牽走。
+            歷史上他們木工與金屬加工的優異，因而提高了<nuxt-link to="building/Siege_Workshop">攻城器製造所</nuxt-link>工作效率/伐木速度/<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>的精良，因離英格蘭很近在牧羊技術上也有相當的程度使得羊不易被順手牽走。
           </p>
           <p>
             <nuxt-link to="civs/Celts">賽爾特</nuxt-link>的後代與英格蘭、愛爾蘭、塞爾特..等文明組成了現今的英國(大不列顛與北愛爾蘭聯合王國)。
@@ -50,7 +50,7 @@
           </tr>
           <tr>
             <td class="unit3">分類文明</td>
-            <td class="unit4">步兵/攻城器</td>
+            <td class="unit4"><nuxt-link to="units/Infantry_Units">步兵</nuxt-link>/<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link></td>
           </tr>
           <tr>
             <td class="unit3"><nuxt-link to="units/Unique_Unit">特殊單位</nuxt-link></td>
@@ -81,25 +81,25 @@
         <div class="n1">
           <ul>
             <li>
-              擁有強大的步兵除了天生的移動速度增加15%步兵的相關科技全滿(除了護衛因為平衡)。
+              擁有強大的<nuxt-link to="units/Infantry_Units">步兵</nuxt-link>除了天生的移動速度增加15%步兵的相關科技全滿(除了<nuxt-link to="techs/Squires">護衛</nuxt-link>因為平衡)。
             </li>
             <li>
-              攻城器器相關科技都有(除了火砲)，此外攻擊速度+20%、血量+40%(帝王<nuxt-link to="techs/untech">特殊科技</nuxt-link><nuxt-link to="techs/Furor_Celtica">賽爾特狂暴</nuxt-link>)、<nuxt-link to="building/Siege_Workshop">攻城器製造所</nuxt-link>工作效率+20%(團隊加分)，因此攻城器的使用是上上選。
+              <nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>相關科技都有(除了<nuxt-link to="units/Bombard_Cannon">火砲</nuxt-link>)，此外攻擊速度+20%、血量+40%(帝王<nuxt-link to="techs/untech">特殊科技</nuxt-link><nuxt-link to="techs/Furor_Celtica">賽爾特狂暴</nuxt-link>)、<nuxt-link to="building/Siege_Workshop">攻城器製造所</nuxt-link>工作效率+20%(團隊加分)，因此<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>的使用是上上選。
             </li>
             <li>
               移動快速的<nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>對建築本身又有加成，適合打騷擾戰術，唯一缺點是本身護甲並不高，因此在有預先防禦下是很難打出一定成果。
             </li>
             <li>
-              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>缺點之一是他們的弓兵系統，欠缺勁弩兵/拇指環/弓兵環甲/護腕這幾項很重要的科技，因此不適合打弓兵。
+              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>缺點之一是他們的<nuxt-link to="units/Archer_Units">弓兵</nuxt-link>系統，欠缺<nuxt-link to="units/Arbalest">勁弩兵</nuxt-link>/<nuxt-link to="techs/Thumb_Ring">拇指環</nuxt-link>/<nuxt-link to="techs/Ring_Archer_Armor">弓兵環甲</nuxt-link>/<nuxt-link to="techs/Bracer">護腕</nuxt-link>這幾項很重要的科技，因此不適合打<nuxt-link to="units/Archer_Units">弓兵</nuxt-link>。
             </li>
             <li>
-              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>的<nuxt-link to="building/Stable">馬廠</nuxt-link>雖然說有著遊戲中最強大騎兵單位遊俠，卻沒有<nuxt-link to="techs/Bloodlines">品種</nuxt-link>/<nuxt-link to="techs/Plate_Barding_Armor">騎兵板甲</nuxt-link>這二樣關鍵科技另外也無駱駝科技。
+              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>的<nuxt-link to="building/Stable">馬廠</nuxt-link>雖然說有著遊戲中最強大騎兵單位<nuxt-link to="units/Paladin">遊俠</nuxt-link>，卻沒有<nuxt-link to="techs/Bloodlines">品種</nuxt-link>/<nuxt-link to="techs/Plate_Barding_Armor">騎兵板甲</nuxt-link>這二樣關鍵科技另外也無駱駝科技。
             </li>
             <li>
-              因為擁有伐木速度+15%的文明加成，跟遊戲中最強大的攻城器，在黑森林這張遊戲地圖中被稱之為最強文明。
+              因為擁有伐木速度+15%的文明加成，跟遊戲中最強大的<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>，在黑森林這張遊戲地圖中被稱之為最強文明。
             </li>
             <li>
-              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>因弓兵系統疲弱、騎兵系統不完善的情況下擁有著最強大的攻城器和強大的步兵系統，幾乎確定是標準的打步兵的文明，因此在某些方面上很容易被針對。
+              <nuxt-link to="civs/Celts">賽爾特</nuxt-link>因<nuxt-link to="units/Archer_Units">弓兵</nuxt-link>系統疲弱、騎兵系統不完善的情況下擁有著最強大的<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>和強大的<nuxt-link to="units/Infantry_Units">步兵</nuxt-link>系統，幾乎確定是標準的打<nuxt-link to="units/Infantry_Units">步兵</nuxt-link>的文明，因此在某些方面上很容易被針對。
             </li>
             <li>
               <nuxt-link to="civs/Celts">賽爾特</nuxt-link>的文明加成，使得<nuxt-link to="civs/Celts">賽爾特</nuxt-link>在黑暗、封建有著不錯的先天優勢。
@@ -112,7 +112,7 @@
                 class="gg"
                 src="~assets/WBRSK.jpg"
                 alt=""
-              /><nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>:快速又具破壞力的步兵
+              /><nuxt-link to="units/Woad_Raider">菘藍武士</nuxt-link>:快速又具破壞力的<nuxt-link to="units/Infantry_Units">步兵</nuxt-link>
             </li>
           </ul>
           <h3><nuxt-link to="techs/untech">特殊科技</nuxt-link></h3>
@@ -136,8 +136,8 @@
         <div class="n1">
           <ul>
             <li>伐木工工作效率提升15%</li>
-            <li>步兵移動速度+15%</li>
-            <li>攻城器發射速度+20%</li>
+            <li><nuxt-link to="units/Infantry_Units">步兵</nuxt-link>移動速度+15%</li>
+            <li><nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>發射速度+20%</li>
             <li>可以在其他玩家單位視野內訓化其他玩家的綿羊</li>
             <li><strong>團隊加分</strong>:<nuxt-link to="building/Siege_Workshop">攻城器製造所</nuxt-link>工作效率提升20%</li>
           </ul>
@@ -155,7 +155,7 @@
             <em><nuxt-link to="/games/aof">失落的帝國</nuxt-link></em>
           </h3>
           <ul>
-            <li><nuxt-link to="techs/Furor_Celtica">賽爾特狂暴</nuxt-link>給予攻城武器40%的生命值加成，而非50%了</li>
+            <li><nuxt-link to="techs/Furor_Celtica">賽爾特狂暴</nuxt-link>給予攻<nuxt-link to="units/Siege_Weapons">攻城器</nuxt-link>40%的生命值加成，而非50%了</li>
             <li>加入城堡時代<nuxt-link to="techs/untech">特殊科技</nuxt-link>：<nuxt-link to="techs/Stronghold">堡壘</nuxt-link>:<nuxt-link to="building/Castle">城堡</nuxt-link>和<nuxt-link to="building/Watch_Tower">箭塔</nuxt-link>攻擊速度+20%</li>
           </ul>
         </div>
